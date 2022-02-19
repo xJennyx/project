ef11f8b672b387affac6a357be39b15fdb65eb8c
Viktor <xjennyx068@gmail.com>
2022-02-19 13:07:58 +0300

    first app practice

--- JS/script.js
+++ JS/script.js
@@ -1,22 +1,21 @@
 "use strict"; 
 
-console.log('arr' + " - object");
-console.log(4 + "5");
+const numberOfFilms = +prompt('Сколько фильмов вы уже просмотретли?', '');
 
-let incr = 10,
-    decr = 10;
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-/* ++incr;
---decr; */
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
 
-console.log(++incr);
-console.log(--decr);
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-console.log(7%2);
-
-console.log(2 + 2 * 2 !== '6');
-
-const isChecked = false,
-      isClose = false;
-
-console.log(isChecked || !isClose);
+console.log(personalMovieDB);
