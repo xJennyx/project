7d624ef98ea892f37f30b9c65f913be68a12e018
Viktor <xjennyx068@gmail.com>
2022-02-21 14:02:26 +0300

    21feb2022

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,6 @@
 "use strict"; 
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже просмотретли?', '');
+/* const numberOfFilms = +prompt('Сколько фильмов вы уже просмотретли?', '');
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -18,4 +18,39 @@ const a = prompt('Один из последних просмотренных ф
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+console.log(personalMovieDB); */
+
+if (1) {
+    console.log('Ok!');
+} else {
+    console.log('Error');
+}
+
+
+
+/* if(num<49) {
+    console.log('Error');
+} else if (num> 100) {
+    console.log('Много');
+} else {
+    console.log('Ok!');
+} */
+
+//(num === 50) ? console.log('Ok!') : console.log('Error');
+
+
+const num = '50';
+switch (num) {
+    case '49':
+        console.log('Неверно');
+        break;
+    case '100':
+        console.log('Неверно');
+        break;
+    case '50':
+        console.log('В точку!');
+        break;       
+    default:
+        console.log('Не в этот раз');
+        break;  
+}
