9a34ea30354b7267f0d3db100e0954af5540e424
Viktor <xjennyx068@gmail.com>
2022-03-15 10:54:29 +0300

    027 Задачи с собеседований на понимание основ

--- JS/script.js
+++ JS/script.js
@@ -1,56 +1,28 @@
 'use strict'; 
 
-// To String
+// let y = 1;
+// let x = y = 2;
+// alert(x);
 
-// 1)
-console.log(typeof(String(null))); 
-console.log(typeof(String(4))); 
+//console.log([] + 1 + 2); // "12"
 
-// 2)
-console.log(typeof(null + ''));
+// alert("1"[0]);
 
+//console.log(2 && 1 && null && 0 && undefined);
+// И запинается на лжи
+// ИЛИ запинается на правде
 
-const num = 5;
+//console.log(!!( 1 && 2 ) === (1 && 2));
+                  //3
+//alert( null || 2 && 3 || 4 ); 
 
-console.log("https://vk.com/catalog/" + num);
+/* const a = [1, 2, 3]; 
+const b = [1, 2, 3];
 
-const fontSize = 26 + 'px';
+console.log(a==b); */
 
-// To Number
+//alert( +"Infinity" ); 
 
-// 1)
-console.log(typeof(Number('4'))); 
+//console.log("Ёжик" > "яблоко"); // false
 
-// 2)
-console.log(typeof(+'5')); 
-
-// 3)
-console.log(typeof(parseInt("15px", 10)));
-
-
-let answ = +prompt("Hello", "");
-
-
- // To boolean
-
- // 0, '', null, undefined, NaN;
-
- // 1)
- let switcher = null;
-
- if (switcher) {
-     console.log('Working...');
- }
-
- switcher = 1;
-
- if (switcher) {
-    console.log('Working...');
-}
-
-// 2)
-console.log(typeof(Boolean('4'))); 
-
-// 3)
-console.log(typeof(!!"4444")); 
-
+//console.log(0 || "" || 2 || undefined || true || falsе);
