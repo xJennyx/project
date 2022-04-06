7b64bac9a084b812423fc28d370cc60a6dd35618
Viktor <xjennyx068@gmail.com>
2022-04-06 09:59:31 +0300

    040 Работа с датами

--- JS/test.js
+++ JS/test.js
@@ -1,3 +1,27 @@
 'use strict';
 
-console.log('Test');
+const now = new Date('2022-04-06');
+//new Date.parse('2022-04-06');
+
+console.log(now.setHours(40));
+console.log(now);
+
+/* console.log(now.getFullYear());
+console.log(now.getMonth());
+console.log(now.getDate());
+console.log(now.getHours());
+console.log(now.getUTCHours()); */
+
+//console.log(now.getTimezoneOffset());
+//console.log(now.getTime());
+
+
+let start = new Date();
+
+for (let i = 0; i < 100000; i++) {
+    let some = i ** 3;
+}
+
+let end = new Date();
+
+alert(`Цикл отработал за ${end - start} милисекунд`);
